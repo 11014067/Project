33bed30ae5b7a62487f5d8d367ca5c31995526e4
11014067 <31650638+11014067@users.noreply.github.com>
2018-06-12 12:07:40 +0200

    .

--- progproj_js.js
+++ progproj_js.js
@@ -47,25 +47,30 @@ function DrawMap() {
 	d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 		if (error) throw error;
 
-	svg.append("g")
-		.attr("class", "states")
-		.selectAll("path")
-		.data(topojson.feature(us, us.objects.states).features)
-		.enter()
-			.append("path")
-			.attr("d", path)
-			.attr("id", function(d) { return d.id });
+		svg.append("g")
+			.attr("class", "states")
+			.selectAll("path")
+			.data(topojson.feature(us, us.objects.states).features)
+			.enter()
+				.append("path")
+				.attr("d", path)
+				.attr("id", function(d) {
+					var name
+					for lengthtxt{ 
+						if d.id == txt.id[i]
+							name = txt[i].name
+					}
+					return name });
 
-	svg.append("path")
-		.attr("class", "state-borders")
-		.attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+		svg.append("path")
+			.attr("class", "state-borders")
+			.attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 	});
 	
 }
 
 // function DrawCalander(){
-	// var 
-	  // d3.tsv("us-state-names.tsv", function(tsv){
+	// var d3.tsv("us-state-names.tsv", function(tsv){
     //// extract just the names and Ids
     // var names = {};
     // tsv.forEach(function(d,i){
